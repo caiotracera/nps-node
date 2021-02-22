7c4d2f3937e31c919970024ab4919641ce83d325
Caio Tracera <ctrcra@gmail.com>
2021-02-22 10:19:53 -0300

    feat: add server main file

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import { errors } from 'celebrate';
+
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(errors());
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+    return response
+      .status(500)
+      .json({ status: 'error', message: 'Internal server errror' });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
